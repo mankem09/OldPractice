400dc47a4769cc10e7e1bec9a7125d7fbbd28e98
M <M@LAPTOP-FG8M1M8N>
2022-05-24 08:08:49 +0530

    updated log4j

--- src/test/java/logintest/TC002_verifySignInPage.java
+++ src/test/java/logintest/TC002_verifySignInPage.java
@@ -2,7 +2,6 @@ package logintest;
 
 import java.io.IOException;
 
-import org.apache.log4j.Logger;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,14 +11,14 @@ import com.pageObjectModel.uiObjects.CreateAccountPageObjects;
 import com.pageObjectModel.uiObjects.SigninPageObjects;
 
 public class TC002_verifySignInPage extends TestBase {
-	public static final Logger Log= Logger.getLogger(TC002_verifySignInPage.class.getName());
+//	public static final Logger Log = Logger.getLogBuilder(TC002_verifySignInPage.class.getName());
 
 	@BeforeClass
 	public void startTest() throws IOException {
 		init();
 	}
 
-	@Test(priority=1)
+	@Test(priority = 1)
 	public void verifySignIn() throws Exception {
 		SigninPageObjects signinpageobjects = new SigninPageObjects(driver);
 		signinpageobjects.clickOnSignIn();
@@ -27,22 +26,20 @@ public class TC002_verifySignInPage extends TestBase {
 		Thread.sleep(5000);
 		signinpageobjects.enterEmail();
 		Thread.sleep(4000);
-		
+
 	}
-	@Test(priority=2)
-	public void verifyCreateAccount(){
-		Log.info("verifyCreate Account started ");
+
+	@Test(priority = 2)
+	public void verifyCreateAccount() {
 		CreateAccountPageObjects createaccountpageobjects = new CreateAccountPageObjects(driver);
 		createaccountpageobjects.personalInformation();
 		createaccountpageobjects.yourAddress();
-		Log.info("verifyCreate acoount ended");
-		
+
 	}
+
 	@AfterClass
 	public void eendtest() {
 		closeBrowser();
 	}
-	
-	
 
 }
